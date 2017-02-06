2f6010c7741b03ca5642aef2850795343785fed2
Troy Madsen <Troy Madsen>
2017-02-06 10:01:16 -0500

    -Initial upload of the Minesweeper branch.
    -Added the MinesweeperGame class that handles all operation.
    
    -Added the Cell class responsible for maintaining the state of each cell
    in the game.
    
    -Added the GameStatus enumeration class that provides the status of the
    game.
    
    -

--- app/src/main/java/timdevries/gamesuite350/Home.java
+++ app/src/main/java/timdevries/gamesuite350/Home.java
@@ -1,20 +1,13 @@
 package timdevries.gamesuite350;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 public class Home extends AppCompatActivity {
 
