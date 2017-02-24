aa0b7c9cc4bb958046870413681636eb8777dd9e
Troy Madsen <troylmadsen@gmail.com>
2017-02-24 18:48:02 -0500

    -Begun building minesweeper GUI

--- app/src/main/java/timdevries/gamesuite350/MinesweeperGUI.java
+++ app/src/main/java/timdevries/gamesuite350/MinesweeperGUI.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import madsen.MinesweeperGame;
 
@@ -24,6 +25,16 @@ public class MinesweeperGUI extends AppCompatActivity {
     private Button[][] board;
 
     /**
+     * Board size.
+     */
+    private final int size = 5;
+
+    /**
+     * son of a beach tree.
+     */
+    private RelativeLayout myLayout;
+
+    /**
      *
      * @param savedInstanceState
      */
@@ -37,10 +48,17 @@ public class MinesweeperGUI extends AppCompatActivity {
         // Creating a new game of MinesweeperGUI
         game = new MinesweeperGame();
 
+        // Initializing board of buttons
+        board = new Button[size][size];
+
+        // Layout of buttons
+        myLayout = (RelativeLayout) findViewById(R.id.content_minesweeper);
+
         // Creating the grid of buttons
         for (int y = 0; y < game.getBoardHeight(); y++) {
             for (int x = 0; x < game.getBoardWidth(); x++) {
                 board[y][x] = new Button(this);
+                myLayout.addView(board[y][x]);
                 board[y][x].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(final View v) {
@@ -50,7 +68,8 @@ public class MinesweeperGUI extends AppCompatActivity {
             }
         }
 
+        this.
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
 }
