0bfaab214f96092747c77e4c6adc7ed0872f5bdf
johnsma7 <johnsma7@mail.gvsu.edu>
2017-04-20 00:43:49 -0400

    CheckersLogic is finished?

--- app/src/test/java/timdevries/gamesuite350/CheckersLogicTests.java
+++ app/src/test/java/timdevries/gamesuite350/CheckersLogicTests.java
@@ -2,6 +2,10 @@ package timdevries.gamesuite350;
 
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * This is the test file for the CheckersLogic class.
@@ -10,12 +14,23 @@ import org.junit.Test;
 
 public class CheckersLogicTests {
 
-    private CheckersLogic cl = new CheckersLogic();
-
-
     @Test
     public void test_move() {
-        cl.getBoard();
 
+        CheckersLogic cl = new CheckersLogic();
+        // move happens
+        assertTrue(cl.move(3, 1, 2, 0));
+    }
+
+    @Test
+    public void test_jump_move() {
+        CheckersLogic cl = new CheckersLogic();
+        // move several times to set up a jump
+        cl.move(3, 1, 2, 0);
+        cl.move(4, 0, 3, 1);
+        cl.move(4, 2, 5, 1);
+        cl.move(5, 1, 6, 2);
+        //the jump
+        assertTrue(cl.move(6, 2, 4, 0));
     }
 }
